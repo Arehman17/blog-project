03943f56074e2e228f7d86e736e27ded79e6701a
Arehman17 <160100971+Arehman17@users.noreply.github.com>
2024-02-15 06:53:12 +0000

    Student Card

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Card from './Component/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Card/>
+      <Card/>
+      <Card/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
