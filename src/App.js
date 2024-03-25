40a4627bf848b0c44db58987450cc7c457a93fc1
Arehman17 <160100971+Arehman17@users.noreply.github.com>
2024-03-25 08:33:42 +0000

    task 9 completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Card from './Component/Card';
-import './App.css';
-import HomePage from './pages/home/HomePage';
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-  <div className= "App font-opensans">
-    <HomePage/>
-      </div>
+    <div className="App font-opensans">
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
+    </div>
   );
 }
 
